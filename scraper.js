485f02a24114b4200e9a091df38a54587231390c
Joey Nguyen <joeylnguyen95@gmail.com>
2021-07-15 05:00:41 -0700

    Scraper clicks on request tour and fills out form

--- scraper.js
+++ scraper.js
@@ -1,8 +1,5 @@
 const puppeteer = require('puppeteer');
 
-
-
-
 const grabInnerText = async (page, selector) => {
   return await page.$eval(selector, element => element.innerText);
 };
@@ -42,6 +39,18 @@ const scrape = async (listingUrl) => {
 
   let availability = await page.evaluate(() => document.querySelectorAll('div.Vitals-data')[0].innerText);
 
+  await page.click('section.rental-contact-box.actions > div.DetailsPage-contact-box.hidden-sm > div.se_embed_react.DetailsPage-ctaButtons > div.styled__ContactBox-sc-1eov7te-0.kTmRnF > div.styled__ButtonsContainer-sc-1nvoegm-2.zUXxC > button.eTgfsf.iFEpWc.jeYSBY.styled__Button-sc-1d5hope-0.styled__ContactButton-sc-1nvoegm-0.styled__PrimaryContactButton-sc-1nvoegm-3');
+
+  await page.focus('div.styled__ContentWrapper-ybwc4h-0.duJiui > div.styled__ModalContent-ybwc4h-2.ejlqiJ > form.styled__Form-f0qfqg-1.ktKBKW > div.styled__FormContent-f0qfqg-2.jxWLJU > div.styled__InputWrapper-sc-164vbn8-0.ChKMb > div.styled__Field-sc-1m3nshm-0.crLbii > input[name=name]');
+
+  await page.keyboard.type(`${process.env.FIRST_NAME} ${process.env.LAST_NAME}`);
+
+  await page.focus('div.styled__ContentWrapper-ybwc4h-0.duJiui > div.styled__ModalContent-ybwc4h-2.ejlqiJ > form.styled__Form-f0qfqg-1.ktKBKW > div.styled__FormContent-f0qfqg-2.jxWLJU > div.styled__InputWrapper-sc-164vbn8-0.ChKMb > div.styled__Field-sc-1m3nshm-0.crLbii > input[name=email]');
+
+  await page.keyboard.type(process.env.EMAIL);
+
+  await page.click('div.styled__ContentWrapper-ybwc4h-0.duJiui > div.styled__ModalContent-ybwc4h-2.ejlqiJ > form.styled__Form-f0qfqg-1.ktKBKW > div.styled__SubmitButtonContainer-b1ssh1-0.bGNILO > button[type=submit]');
+
   await browser.close();
 
   return {
@@ -51,7 +60,7 @@ const scrape = async (listingUrl) => {
       "Monthly Rent": price,
       "Net Effective Rent": netPrice,
       "Neighborhood": neighborhood,
-      "Contacted": false,
+      "Contacted": true,
       "Size": size,
       "Availability": availability
     }
